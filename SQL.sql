27460bda58b19215d39d9343a12bfe86be2fe25f
NataschaBGB <88776983+NataschaBGB@users.noreply.github.com>
2022-03-28 14:52:15 +0200

    Add files via upload

---
+++ SQL.sql
@@ -0,0 +1,18 @@
+﻿USE [SQL-Opgave];
+
+CREATE TABLE Gryffindor
+(
+    PersonID int,
+    FirstName varchar(255),
+    LastName varchar(255),
+    Age int
+);
+
+INSERT INTO Gryffindor
+VALUES (1, 'Harry', 'Pooper', 15);
+
+INSERT INTO Gryffindor
+VALUES (2, 'Hemoride', 'Granger', 15);
+
+INSERT INTO Gryffindor
+VALUES (3, 'Ron', 'Weasel', 15);
